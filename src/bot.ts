13bfe0b57a64c7bb92f1d090a685f831e8913ed0
Floemia <mcescobar73@gmail.com>
2025-04-19 14:59:25 -0400

    Fix todo, release que parece funcionar

--- src/bot.ts
+++ src/bot.ts
@@ -8,62 +8,66 @@ let token = process.env.DISCORD_TOKEN;
 if (!token) throw new Error('process.env.DISCORD_TOKEN == undefined');
 
 utils.log({
-  prefix: '[BOT]',
-  message: 'Variables de entorno cargadas. Iniciando...',
-  important: true,
-  color: 'Green',
+	prefix: '[BOT]',
+	message: 'Variables de entorno cargadas. Iniciando...',
+	important: true,
+	color: 'Green',
 });
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
-  ],
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
 });
 
 client.once('ready', () => {
-  utils.log({
-    prefix: '[BOT]',
-    message: `${client.user?.username} está online!`,
-    color: 'Green',
-  });
+	utils.log({
+		prefix: '[BOT]',
+		message: `${client.user?.username} está online!`,
+		color: 'Green',
+	});
 });
 
 client.on('messageCreate', async message => {
-  if (
-    !message.content.startsWith('!mc') ||
-    message.author.bot ||
-    message.channel.isDMBased()
-  )
-    return;
-  const args = message.content.split(' ');
-  const command = args[1];
-  if (!command) return;
-  switch (command.toLowerCase()) {
-    case 'start':
-      await server.start(message);
-      break;
-    case 'stop':
-      await server.stop(message);
-      break;
-    case 'restart':
-      await server.restart(message);
-      break;
-    case 'ip':
-      await server.ip(message);
-      break;
-  }
+	if (
+		!message.content.startsWith('!mc') ||
+		message.author.bot ||
+		message.channel.isDMBased()
+	)
+		return;
+	const args = message.content.split(' ');
+	const command = args[1];
+	if (!command) return;
+	switch (command.toLowerCase()) {
+		case 'start':
+			await server.start(message);
+			break;
+		case 'stop':
+			await server.stop(message);
+			break;
+		case 'restart':
+			await server.restart(message);
+			break;
+		case 'ip':
+			await server.ip(message);
+			break;
+	}
 });
 
 client.on('error', err => {
-  utils.log({
-    prefix: '[BOT][ERROR]',
-    message: `Error en el cliente: ${err}`,
-    color: 'Red',
-    important: true,
-  });
+	utils.log({
+		prefix: '[BOT][ERROR]',
+		message: `Error en el cliente: ${err}`,
+		color: 'Red',
+		important: true,
+	});
 });
 
 client.login(token);
+
+process.on('SIGINT', server.handleExit);     // Ctrl+C
+process.on('SIGTERM', server.handleExit);    // kill command
+process.on('exit', server.handleExit);       // any other exit
--- src/server.ts
+++ src/server.ts
@@ -9,362 +9,388 @@ let cache_ip: string | null = null;
 let server_open = false;
 
 const getIp = async (): Promise<string | null> => {
-  if (cache_ip) return cache_ip;
-  try {
-    const response = await fetch('https://api.ipify.org?format=json');
-    if (!response.ok) {
-      utils.log({
-        prefix: '[BOT][ERROR]',
-        message: `Error al obtener la IP: ${response.statusText}`,
-        color: 'Red',
-        important: true,
-      });
-      return null;
-    }
-    const data = await response.json();
-    cache_ip = data.ip;
-    return cache_ip;
-  } catch (err) {
-    utils.log({
-      prefix: '[BOT][ERROR]',
-      message: `Error al obtener la IP: ` + err,
-      color: 'Red',
-      important: true,
-    });
+	if (cache_ip) return cache_ip;
+	try {
+		const response = await fetch('https://api.ipify.org?format=json');
+		if (!response.ok) {
+			utils.log({
+				prefix: '[BOT][ERROR]',
+				message: `Error al obtener la IP: ${response.statusText}`,
+				color: 'Red',
+				important: true,
+			});
+			return null;
+		}
+		const data = await response.json();
+		cache_ip = data.ip;
+		return cache_ip;
+	} catch (err) {
+		utils.log({
+			prefix: '[BOT][ERROR]',
+			message: `Error al obtener la IP: ` + err,
+			color: 'Red',
+			important: true,
+		});
 
-    console.error(err);
-  }
-  return null;
+		console.error(err);
+	}
+	return null;
 };
 
 const serverProcess = () => {
-  let jar_name = process.env.JAR_NAME;
-  let jar_path = process.env.JAR_PATH;
-  let mem = process.env.MEMORY;
+	let jar_name = process.env.JAR_NAME;
+	let jar_path = process.env.JAR_PATH;
+	let mem = process.env.MEMORY;
 
-  if (!jar_path) throw new Error('process.env.JAR_PATH == undefined');
-  if (!jar_name) throw new Error('process.env.JAR_NAME == undefined');
-  if (!mem) throw new Error('process.env.MEMORY == undefined');
+	if (!jar_path) throw new Error('process.env.JAR_PATH == undefined');
+	if (!jar_name) throw new Error('process.env.JAR_NAME == undefined');
+	if (!mem) throw new Error('process.env.MEMORY == undefined');
 
-  mcProcess = spawn('java', [`-Xmx${mem}G`, '-jar', jar_name], {
-    cwd: jar_path,
-    windowsHide: true,
-    shell: true,
-  });
+	mcProcess = spawn('java', [`-Xmx${mem}G`, '-jar', jar_name], {
+		cwd: jar_path,
+		windowsHide: true,
+		shell: true,
+	});
 
-  mcProcess.on('error', err => {
-    utils.log({
-      prefix: '[BOT][ERROR]',
-      message: `El proceso ha tenido un error.`,
-      color: 'Red',
-      important: true,
-    });
+	mcProcess.on('error', err => {
+		utils.log({
+			prefix: '[BOT][ERROR]',
+			message: `El proceso ha tenido un error.`,
+			color: 'Red',
+			important: true,
+		});
 
-    console.error(err);
-    utils.killProcess(mcProcess);
-    server_open = false;
-    mcProcess = null;
-    cache_ip = null;
-    return;
-  });
+		console.error(err);
+		utils.killProcess(mcProcess);
+		server_open = false;
+		mcProcess = null;
+		cache_ip = null;
+		return;
+	});
 
-  mcProcess.on('exit', async (code, signal) => {
-    utils.log({
-      prefix: '[BOT]',
-      message: `Servidor detenido con código ${code} y señal ${signal}`,
-      color: 'Red',
-      important: true,
-    });
+	mcProcess.on('exit', async (code, signal) => {
+		utils.log({
+			prefix: '[BOT]',
+			message: `Proceso detenido con código ${code} y señal ${signal}`,
+			color: 'Red',
+			important: true,
+		});
 
-    server_open = false;
-    mcProcess = null;
-    cache_ip = null;
-  });
+		server_open = false;
+		mcProcess = null;
+		cache_ip = null;
+	});
 
-  mcProcess.stdout.on('data', data => {
-    if (!server_open && data.includes('[Server thread/INFO]: Done'))
-      server_open = true;
-    utils.log({
-      prefix: '[MC]',
-      message: data,
-      color: 'Yellow',
-    });
-  });
+	mcProcess.stdout.on('data', data => {
+		if (!server_open && data.includes('[Server thread/INFO]: Done'))
+			server_open = true;
+		let msg = data.toString().trim();
+		msg.replace(/^\[\d{2}:\d{2}:\d{2}\] /g, '')
+		utils.log({
+			prefix: '[MC]',
+			message: msg,
+			color: 'Yellow',
+		});
+	});
 
-  mcProcess.stderr.on('data', data => {
-    utils.log({
-      prefix: '[MC][ERROR]',
-      message: data,
-      color: 'Red',
-    });
-  });
+	mcProcess.stderr.on('data', data => {
+		let msg = data.toString().trim();
+		msg.replace(/^\[\d{2}:\d{2}:\d{2}\] /g, '')
+		utils.log({
+			prefix: '[MC][ERROR]',
+			message: msg,
+			color: 'Red',
+		});
+	});
 
-  mcProcess.on('spawn', () => {
-    utils.log({
-      prefix: '[BOT]',
-      message: `Servidor iniciado con éxito con PID: ${mcProcess!.pid}`,
-      color: 'Green',
-      important: true,
-    });
-  });
+	mcProcess.on('spawn', () => {
+		utils.log({
+			prefix: '[BOT]',
+			message: `Proceso iniciado con éxito con PID: ${mcProcess!.pid}`,
+			color: 'Green',
+			important: true,
+		});
+	});
 };
 
 const startServer = async () => {
-  if (!mcProcess) {
-    server_open = false;
-    utils.log({
-      prefix: '[BOT]',
-      message: 'Iniciando el servidor...',
-      color: 'Green',
-      important: true,
-    });
-    serverProcess();
+	if (!mcProcess) {
+		serverProcess();
+		server_open = false;
+		utils.log({
+			prefix: '[BOT]',
+			message: 'Iniciando el servidor...',
+			color: 'Green',
+			important: true,
+		});
+		
 
-    let opened = await waitForTrue(() => server_open);
-    if (!opened) {
-      utils.log({
-        prefix: '[BOT]',
-        message: 'No se pudo iniciar el servidor. **Revisar logs!**',
-        color: 'Red',
-        important: true,
-      });
-    }
-    return opened;
-  } else {
-    utils.log({
-      prefix: '[BOT]',
-      message: 'El servidor se intentó iniciar pero ya estaba en ejecución.',
-      color: 'Yellow',
-      important: true,
-    });
-    return true;
-  }
+		let opened = await waitForTrue(() => server_open);
+		if (!opened) {
+			utils.log({
+				prefix: '[BOT]',
+				message: 'No se pudo iniciar el servidor. Timeout',
+				color: 'Red',
+				important: true,
+			});
+			mcProcess = null;
+			cache_ip = null;
+		}
+		return opened;
+	} else {
+		utils.log({
+			prefix: '[BOT]',
+			message: 'El servidor se intentó iniciar pero ya estaba en ejecución.',
+			color: 'Yellow',
+			important: true,
+		});
+		return true;
+	}
 };
 
 const restartServer = async () => {
-  if (mcProcess && !mcProcess.killed) {
-    await stopServer();
-    utils.log({
-      prefix: '[BOT]',
-      message: 'Reiniciando el servidor...',
-      color: 'Green',
-      important: true,
-    });
-    return await startServer();
-  } else {
-    utils.log({
-      prefix: '[BOT]',
-      message: 'El servidor se intentó reiniciar pero no estaba en ejecución.',
-      color: 'Yellow',
-      important: true,
-    });
-    return false;
-  }
+	if (!mcProcess) return utils.log({ prefix: '[BOT]', message: 'El servidor se intentó reiniciar pero no estaba en ejecución.', color: 'Yellow', important: true });
+	utils.log({
+		prefix: '[BOT]',
+		message: 'Reiniciando el servidor...',
+		color: 'Green',
+		important: true,
+	});
+
+	await stopServer();
+	return await startServer();
 };
 
-const stopServer = async () => {
-  if (mcProcess && !mcProcess.killed) {
-    server_open = false;
+const stopServer = async (): Promise<void> => {
+	return new Promise(async (resolve) => {
+		if (!mcProcess) {
+			utils.log({
+				prefix: '[BOT]',
+				message: 'El servidor se intentó detener pero no estaba en ejecución.',
+				color: 'Yellow',
+				important: true,
+			})
+			return resolve();
+		}
 
-    utils.log({
-      prefix: '[BOT]',
-      message: 'Deteniendo el servidor...',
-      color: 'Red',
-      important: true,
-    });
+		utils.log({
+			prefix: '[BOT]',
+			message: 'Deteniendo el servidor...',
+			color: 'Red',
+			important: true,
+		});
 
-    utils.killProcess(mcProcess);
-    mcProcess.once('exit', () => {
-      utils.log({
-        prefix: '[BOT]',
-        message: 'Servidor detenido.',
-        color: 'Red',
-        important: true,
-      });
+		await utils.killProcess(mcProcess);
 
-      mcProcess = null;
-      cache_ip = null;
-    });
-  } else {
-    utils.log({
-      prefix: '[BOT]',
-      message: 'El servidor se intentó detener pero no estaba en ejecución.',
-      color: 'Yellow',
-      important: true,
-    });
-  }
-};
+		mcProcess.once('exit', () => {
+			utils.log({
+				prefix: '[BOT]',
+				message: 'Servidor detenido.',
+				color: 'Red',
+				important: true,
+			});
+
+			server_open = false;
+			mcProcess = null;
+			cache_ip = null;
+			resolve();
+		});
+		mcProcess.kill();
+	});
+}
+
+const handleExit = async () => {
+	if (mcProcess) {
+		await stopServer();
+		mcProcess.kill();
+	}
+	process.exit();
+}
 
 const waitForTrue = async (
-  checkFn: () => boolean,
-  timeout = 25000,
-  interval = 500
+	checkFn: () => boolean,
+	timeout = 120000,
+	interval = 200
 ): Promise<boolean> => {
-  const start = Date.now();
-  return new Promise(resolve => {
-    const check = () => {
-      if (checkFn()) return resolve(true);
-      if (Date.now() - start > timeout) return resolve(false);
-      setTimeout(check, interval);
-    };
-    check();
-  });
+	const start = Date.now();
+	return new Promise(resolve => {
+		const check = () => {
+			if (checkFn()) return resolve(true);
+			if (Date.now() - start > timeout) return resolve(false);
+			setTimeout(check, interval);
+		};
+		check();
+	});
 };
 
 const start = async (message: OmitPartialGroupDMChannel<Message>) => {
-  if (mcProcess)
-    return await message.reply({
-      embeds: [
-        utils.embed({
-          description: 'El servidor ya está en ejecución.',
-          color: 'Yellow',
-        }),
-      ],
-    });
+	if (mcProcess)
+		return await message.reply({
+			allowedMentions: { repliedUser: false },
+			embeds: [
+				utils.embed({
+					description: 'El servidor ya está en ejecución.',
+					color: 'Yellow',
+				}),
+			],
+		});
 
-  let response = await message.reply({
-    embeds: [
-      utils.embed({ description: 'Iniciando servidor...', color: 'White' }),
-    ],
-  });
+	let response = await message.reply({
+		allowedMentions: { repliedUser: false },
+		embeds: [
+			utils.embed({ description: 'Iniciando servidor...', color: 'White' }),
+		],
+	});
+	const timer = utils.createTimer();
+	let success = await startServer();
+	if (!success) {
+		mcProcess = null;
+		cache_ip = null;
+		return await response.edit({
+			embeds: [
+				utils.embed({
+					description: 'No se pudo iniciar el servidor. **Revisar logs!**',
+					color: 'Red',
+				}),
+			],
+		});
+	}
 
-  let success = await startServer();
-  if (!success)
-    return await response.edit({
-      embeds: [
-        utils.embed({
-          description: 'No se pudo iniciar el servidor. **Revisar logs!**',
-          color: 'Red',
-        }),
-      ],
-    });
+	let ip = await getIp();
+	const ipExists = ip ? true : false;
+	let options: EmbedCreatorParameters = {
+		color: ipExists ? 'Green' : 'Red',
+		description:
+			ipExists ?
+				`Servidor iniciado con éxito en \`${timer()}\`. \n> **・IP:** \`${ip}\``
+				: 'No se pudo obtener la IP del servidor . **Revisar logs!**',
+	};
 
-  let ip = await getIp();
-  const ipExists = ip ? true : false;
-  let options: EmbedCreatorParameters = {
-    color: ipExists ? 'Green' : 'Red',
-    description:
-      ipExists ?
-        `Servidor iniciado con éxito. \n\n> **・IP:** \`${ip}\``
-      : 'No se pudo obtener la IP del servidor. **Revisar logs!**',
-  };
-
-  await response.edit({
-    embeds: [utils.embed(options)],
-  });
+	await response.edit({
+		embeds: [utils.embed(options)],
+	});
 };
 
 const restart = async (message: OmitPartialGroupDMChannel<Message>) => {
-  if (!utils.check_permissions(message.member!))
-    return await message.reply({
-      embeds: [
-        utils.embed({
-          description: 'No tenés permiso para usar este comando.',
-          color: 'Red',
-        }),
-      ],
-    });
-  if (!mcProcess)
-    return await message.reply({
-      embeds: [
-        utils.embed({
-          description: 'El servidor no está en ejecución.',
-          color: 'Yellow',
-        }),
-      ],
-    });
-  let response = await message.reply({
-    embeds: [
-      utils.embed({ description: 'Reiniciando servidor...', color: 'White' }),
-    ],
-  });
-  server_open = false;
-  let success = await restartServer();
-  if (!success)
-    return await response.edit({
-      embeds: [
-        utils.embed({
-          description: 'No se pudo reiniciar el servidor. **Revisar logs!**',
-          color: 'Red',
-        }),
-      ],
-    });
+	if (!utils.check_permissions(message.member!))
+		return await message.reply({
+			allowedMentions: { repliedUser: false },
+			embeds: [
+				utils.embed({
+					description: 'No tenés permiso para usar este comando.',
+					color: 'Red',
+				}),
+			],
+		});
+	if (!mcProcess)
+		return await message.reply({
+			allowedMentions: { repliedUser: false },
+			embeds: [
+				utils.embed({
+					description: 'El servidor no está en ejecución.',
+					color: 'Yellow',
+				}),
+			],
+		});
+	let response = await message.reply({
+		allowedMentions: { repliedUser: false },
+		embeds: [
+			utils.embed({ description: 'Reiniciando servidor...', color: 'White' }),
+		],
+	});
+	const timer = utils.createTimer();
+	let success = await restartServer();
+	if (!success)
+		return await response.edit({
+			embeds: [
+				utils.embed({
+					description: 'No se pudo reiniciar el servidor. **Revisar logs!**',
+					color: 'Red',
+				}),
+			],
+		});
 
-  let ip = await getIp();
-  const ipExists = ip ? true : false;
-  let options: EmbedCreatorParameters = {
-    color: ipExists ? 'Green' : 'Red',
-    description:
-      ipExists ?
-        `Servidor reiniciado con éxito. \n\n> **・IP:** \`${ip}\``
-      : 'No se pudo obtener la IP del servidor. **Revisar logs!**',
-  };
+	let ip = await getIp();
+	const ipExists = ip ? true : false;
+	let options: EmbedCreatorParameters = {
+		color: ipExists ? 'Green' : 'Red',
+		description:
+			ipExists ?
+				`Servidor reiniciado con éxito en \`${timer()}\`.\n> **・IP:** \`${ip}\``
+				: 'No se pudo obtener la IP del servidor. **Revisar logs!**',
+	};
 
-  await response.edit({ embeds: [utils.embed(options)] });
+	await response.edit({ embeds: [utils.embed(options)] });
 };
 
 const stop = async (message: OmitPartialGroupDMChannel<Message>) => {
-  if (!utils.check_permissions(message.member!))
-    return await message.reply({
-      embeds: [
-        utils.embed({
-          description: 'No tenés permiso para usar este comando.',
-          color: 'Red',
-        }),
-      ],
-    });
-  if (!mcProcess)
-    return await message.reply({
-      embeds: [
-        utils.embed({
-          description: 'El servidor no está en ejecución.',
-          color: 'Yellow',
-        }),
-      ],
-    });
+	if (!utils.check_permissions(message.member!))
+		return await message.reply({
+			allowedMentions: { repliedUser: false },
+			embeds: [
+				utils.embed({
+					description: 'No tenés permiso para usar este comando.',
+					color: 'Red',
+				}),
+			],
+		});
+	if (!mcProcess)
+		return await message.reply({
+			allowedMentions: { repliedUser: false },
+			embeds: [
+				utils.embed({
+					description: 'El servidor no está en ejecución.',
+					color: 'Yellow',
+				}),
+			],
+		});
 
-  let response = await message.reply({
-    embeds: [
-      utils.embed({ description: 'Deteniendo servidor...', color: 'White' }),
-    ],
-  });
-  stopServer();
+	let response = await message.reply({
+		allowedMentions: { repliedUser: false },
+		embeds: [
+			utils.embed({ description: 'Deteniendo servidor...', color: 'White' }),
+		],
+	});
+	const timer = utils.createTimer();
+	await stopServer();
 
-  response.edit({
-    embeds: [utils.embed({ description: 'Servidor detenido.', color: 'Red' })],
-  });
+	response.edit({
+		embeds: [utils.embed({ description: `Servidor detenido en \`${timer()}\`.`, color: 'Red' })],
+	});
 };
 
 const ip = async (message: OmitPartialGroupDMChannel<Message>) => {
-  if (!mcProcess)
-    return await message.reply({
-      embeds: [
-        utils.embed({
-          description: 'El servidor no está en ejecución.',
-          color: 'Yellow',
-        }),
-      ],
-    });
-  let response = await message.reply({
-    embeds: [
-      utils.embed({
-        description: 'Obteniendo IP del servidor...',
-        color: 'White',
-      }),
-    ],
-  });
-  let ip = await getIp();
-  const ipExists = ip ? true : false;
+	if (!mcProcess)
+		return await message.reply({
+			allowedMentions: { repliedUser: false },
+			embeds: [
+				utils.embed({
+					description: 'El servidor no está en ejecución.',
+					color: 'Yellow',
+				}),
+			],
+		});
+	let response = await message.reply({
+		allowedMentions: { repliedUser: false },
+		embeds: [
+			utils.embed({
+				description: 'Obteniendo IP del servidor...',
+				color: 'White',
+			}),
+		],
+	});
+	let ip = await getIp();
+	const ipExists = ip ? true : false;
 
-  let options: EmbedCreatorParameters = {
-    color: ipExists ? 'Green' : 'Red',
-    description:
-      ipExists ?
-        `La IP del servidor es: \`${ip}\``
-      : 'No se pudo obtener la IP del servidor. **Revisar logs!**',
-  };
+	let options: EmbedCreatorParameters = {
+		color: ipExists ? 'Green' : 'Red',
+		description:
+			ipExists ?
+				`La IP del servidor es: \`${ip}\``
+				: 'No se pudo obtener la IP del servidor. **Revisar logs!**',
+	};
 
-  await response.edit({ embeds: [utils.embed(options)] });
+	await response.edit({ embeds: [utils.embed(options)] });
 };
 
-export const server = { start, restart, stop, ip, mcProcess };
+export const server = { start, restart, stop, ip, mcProcess, handleExit };
